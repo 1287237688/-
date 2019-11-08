c919d02b06c939aba26f4e119ed38260d90ce770
1287237688 <51707476+1287237688@users.noreply.github.com>
2019-11-08 16:37:28 +0800

    Update 链表指定位置输出符号结束.cpp

--- 链表指定位置输出符号结束.cpp
+++ 链表指定位置输出符号结束.cpp
@@ -1,36 +1,56 @@
 #include<stdio.h>
-#include<stdlib.h>
-struct node{
-    int date;
-    struct node* next;
-}; 
-int main()
+#define maxsize 100
+typedef int elemtype;
+typedef struct
 {
-    struct node* list=(struct node*)malloc(sizeof(struct node));
-    list->date=NULL;     
-    list->next=NULL;
-    int k;
-    scanf("%d",&k);
-    int date;
-    while(scanf("%d",&date)!=EOF&&date>=0)
-    { 
-        struct node* newnode=(struct node*)malloc(sizeof(struct node));
-        newnode->date=date;
-        newnode->next=NULL;
-        newnode->next=list->next;
-        list->next=newnode;
-    }
-    int count=1;
-    struct node* p=list->next;
-    while(p!=NULL)
-    {
-        if(count==k)
-        break;
-        count++;
-        p=p->next;
-    } 
-    if(p!=NULL)
-     printf("%d",p->date);
-    else
-     printf("NULL");
+	elemtype elem[maxsize];
+	int length;
+}Sqlist;
+void Initlist(Sqlist &L) // 初始化表 L
+{
+	L.length=0; 
+}
+void Creatlist(Sqlist &L)
+{
+	int i;
+	scanf("%d\n",&L.length);
+	for(i=0;i<L.length;i++)
+	scanf("%d",&L.elem[i]);
+	for(i=0;i<L.length;i++)
+	printf("%5",L.elem[i]);
+}
+void Insertlist(Sqlist &L,int j,int e) // 插入操作
+{
+	int i;
+	for(i=L.length;i>=j-1;i--)
+	L.elem[i+1]=L.elem[i];
+	L.elem[j-1]=e;
+	L.length=L.length+1;
+}
+void Deletelist(Sqlist &L,int j) // 删除元素
+{
+	int i;
+	for(i=j-1;i<L.length-1;i++)
+	L.elem[i]=L.elem[i+1];
+	L.length=L.length-1;
+	printf(" 新的线性表序列为： ");
+	for(i=0;i<L.length;i++)
+	printf("%3d",L.elem[i]);
+	printf("\n");
+}
+int main() 
+{
+	Sqlist L;
+	int j,e;
+	printf(" 请定义表长和元素： ");
+	Initlist(L);
+	Creatlist(L);
+	printf(" 请输入插入的位置： ");
+	scanf("%d",&j); 
+	printf(" 请输入插入的元素： ");
+	scanf("%d",&e);
+	Insertlist(L,j,e);
+	printf(" 请输入删除的元素： ");
+	scanf("%d",&j);
+	Deletelist(L,j);
 }
